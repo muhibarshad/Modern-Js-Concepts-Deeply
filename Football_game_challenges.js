f0b517996dd3f826acc2dc5d48b2b2ae3801ce0a
Muhib Arshad <muhibarshad123@gmail.com>
2022-07-01 19:03:39 +0500

    Todays challenge Done

--- Football_game_challenges.js
+++ Football_game_challenges.js
@@ -95,4 +95,34 @@ for (const player of game.scored) {
 console.log(scorers);
 
 
+//==============================================
+//Third coding challenges
+//==============================================
 
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+  ]);
+
+//1-
+const events=[...new Set(gameEvents.values())];
+console.log(events);
+//2-
+gameEvents.delete(64);
+console.log(gameEvents);
+//3-
+let time=[...gameEvents.keys()].pop();
+console.log(`An event happened ,on average ,on every ${time/gameEvents.size} minutes`);
+//4-
+for(const [key,value] of gameEvents){
+     console.log(`[${key<=45?'First Half':'Second Half'}] ${key} : ${value}`);
+}
