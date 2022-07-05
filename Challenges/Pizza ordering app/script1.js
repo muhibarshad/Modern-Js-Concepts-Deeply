0c4fce9bfdf8ee3012abe1382e1d3f4ea771faf0
Muhib Arshad <muhibarshad123@gmail.com>
2022-07-05 23:09:23 +0500

    order

--- Challenges/Pizza ordering app/script1.js
+++ Challenges/Pizza ordering app/script1.js
@@ -1,28 +1,76 @@
 "use strict";
-const pizza = document.querySelectorAll("td");
+
+//varaibles
+const pizzaMenu = document.querySelectorAll("td");
 const overlay = document.querySelector(".size");
 const okCancel = document.querySelectorAll(".btn");
 const sizeMenu = document.querySelectorAll(".s");
-
+const cancel=document.querySelector('.cancel');
+const ok=document.querySelector('.OK');
+const price=document.querySelector('.price');
+const orderNow=document.querySelector('.order');
+const cancelOrder=document.querySelector('.cancelOrder')
+const pricesPizza=[
+  [600,500,350,400,320,250],
+  [800,650,550,630,720,670],
+  [1200,990,1050,890,1000,800]
+]
+let pizzaIndex=0;
+//supporting-functions
 const modelShow = function () {
   overlay.classList.toggle("hidden");
 };
+const deafultSelection=function(){
+  for (let i = 0; i < sizeMenu.length; i++) {
+    sizeMenu[i].style.backgroundColor = "rgb(202, 124, 0)";
+  }
+};
+const defaultStarter=function(){
+  sizeMenu[2].style.backgroundColor = "green";
+}
 
-//show model
-for (const [index, ele] of pizza.entries())
-  pizza[index].addEventListener("click", modelShow);
+//default starters
+defaultStarter();
+
+//show model-pizzaSelection
+for (const [index, ele] of pizzaMenu.entries()){
+  pizzaMenu[index].addEventListener("click", modelShow);
+  pizzaMenu[index].addEventListener('click',function(){
+  let pizzaName=pizzaMenu[index].innerText;
+  price.innerText=pricesPizza[2][index];
+  pizzaIndex=index;
+  console.log(pizzaName);
+  document.querySelector('.name').innerText=pizzaName;
+  })
+}
+
 
 //remove model
 for (const [index, ele] of okCancel.entries())
   okCancel[index].addEventListener("click", modelShow);
 
+//selectSize
 for (const [index, ele] of sizeMenu.entries()) {
   sizeMenu[index].addEventListener("click", function () {
-    for (let i = 0; i < sizeMenu.length; i++) {
-      sizeMenu[i].style.backgroundColor = "rgb(202, 124, 0)";
-    }
-    sizeMenu[index].style.backgroundColor == "green"
-      ? (sizeMenu[index].style.backgroundColor = "rgb(202, 124, 0)")
-      : (sizeMenu[index].style.backgroundColor = "green");
+    deafultSelection();
+    sizeMenu[index].style.backgroundColor = "green";
+    price.innerText=pricesPizza[index][pizzaIndex];
+
   });
 }
+//OnCancel
+cancel.addEventListener('click',function(){
+  deafultSelection();
+  defaultStarter();
+});
+//onOk
+ok.addEventListener('click',function(){
+   pizzaMenu[pizzaIndex].classList.toggle('selected');
+   orderNow.classList.toggle('confirm');
+
+});
+//cancelOrder
+cancelOrder.addEventListener('click',function(){
+  pizzaMenu[pizzaIndex].classList.toggle('selected');
+  orderNow.classList.toggle('confirm');
+})
