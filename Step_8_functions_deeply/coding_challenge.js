d8efdb93df30b81c69d5e52a3995857333c59b29
Muhib Arshad <muhibarshad123@gmail.com>
2022-07-05 07:39:12 +0500

    Update coding_challenge.js

--- Step_8_functions_deeply/coding_challenge.js
+++ Step_8_functions_deeply/coding_challenge.js
@@ -5,7 +5,6 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
-
   registerNewAnswer() {
     //prompt
     const userOption = Number(
