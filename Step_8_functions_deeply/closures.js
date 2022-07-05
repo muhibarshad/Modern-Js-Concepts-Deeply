0a0098295748900d2125acd663e6061d47228678
Muhib Arshad <muhibarshad123@gmail.com>
2022-07-05 09:52:48 +0500

    Functions portion Completed

--- Step_8_functions_deeply/closures.js
+++ Step_8_functions_deeply/closures.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 //=====================================================
 /*Defination:
     Functions have an access to its birthPlace variable enviornment always ,even 
@@ -14,19 +14,20 @@ Explanation:
 */
 //=====================================================
 
-const patientChecked=function(){
-    let patientCount=0;
-    return function(){
-        patientCount++;
-        console.log(`Total ${patientCount} has been checked.`);
-    }
-}
+//Example:01
+const patientChecked = function () {
+  let patientCount = 0;
+  return function () {
+    patientCount++;
+    console.log(`Total ${patientCount} has been checked.`);
+  };
+};
 
-const patientScreen=patientChecked();
+const patientScreen = patientChecked();
 
-patientScreen();//1
-patientScreen();//2
-patientScreen();//3
+patientScreen(); //1
+patientScreen(); //2
+patientScreen(); //3
 
 /*Number are increasing ,but if we see that in patientScreen ,patientChecked function
 has been interpreted and gone off from the stack and also be removed from the scope chain.
@@ -41,3 +42,35 @@ but we cannot manully access them.
 //To get the scope and closures
 console.dir(patientScreen);
 //[[]]------means we cannot access them in our code but happens in bheind the scenes
+
+//Example:02
+const boardingPassengers = function (n, wait) {
+  let perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`we have an ${n} of passengers`);
+    console.log(`We have an ${perGroup} of passengers`);
+  }, wait * 1000);
+  console.log(`We all boarding the passengers in the ${wait} seconds`);
+};
+
+boardingPassengers(180, 2);
+/**********Imporatnt Note*************
+ functions closures have an priority over the scope chain.means firstly tke local 
+ functions are to be accessed then access to the global varibels 
+ */
+
+//Closure Challenge
+
+(function () {
+  const header = document.querySelector(".h1");
+  header.style.color = "red";
+  document.querySelector('body').addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
+
+//explain
+/*IIFE is invoked imediatley ,and popped off from the stack then by clicking on the header 
+callback function is invoked but it get the header varaible sfrom the IIFE ,so this is the very 
+interesting example of the closures
+*/
