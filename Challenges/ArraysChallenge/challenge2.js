bdb1451e6f76c42ca12c658be6bc117e13ec1b96
Muhib Arshad <muhibarshad123@gmail.com>
2022-07-18 15:08:31 +0500

    Update challenge2.js

--- Challenges/ArraysChallenge/challenge2.js
+++ Challenges/ArraysChallenge/challenge2.js
@@ -4,7 +4,7 @@ let data2 = [16, 6, 10, 5, 6, 1, 4];
 const calcAverageHumanAge = function (ages) {
   const humanYearsAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
   const finalData = humanYearsAges.filter((age) => age >= 18);
-  let average = finalData.reduce((acc, age) => acc + age) / finalData.length;
+  let average = finalData.reduce((acc, age) => acc + age,0) / finalData.length;
   return `The average = ${average}`;
 };
 console.log(calcAverageHumanAge(data1));
