292467459e3cabd67cf0ec9b0b8780f3a52e1b9c
Muhib Arshad <muhibarshad123@gmail.com>
2022-08-29 00:50:01 +0500

    Revision

---
+++ temp.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+dogs.forEach((value) => {
+  value.recommendedFood = Math.trunc(value.weight ** 0.75 * 28);
+});
+let str = (na, str, cur, rec) => {
+  na = na.replace(na[0], na[0].toUpperCase());
+  console.log(
+    `${na} dog's is eating too ${str} beacuse its current food is ${cur} but the recommended food is ${rec}`
+  );
+};
+dogs.find((value) => {
+  let x = value.owners
+    .filter((nam) => nam === "Sarah")
+    .join("")
+    .toLowerCase();
+  x === "sarah" &&
+    value.curFood > value.recommendedFood &&
+    str(x, "much", value.curFood, value.recommendedFood);
+  x === "sarah" &&
+    value.curFood < value.recommendedFood &&
+    str(x, "little", value.curFood, value.recommendedFood);
+});
+
+const ownersEatTooMuch = dogs
+  .filter((value) => value.curFood > value.recommendedFood)
+  .flatMap((value) => value.owners);
+const ownersEatTooLittle = dogs
+  .filter((value) => value.curFood < value.recommendedFood)
+  .flatMap((value) => value.owners);
+
+ownersEatTooMuch.forEach((value, _, arr) => {
+  console.log(
+    `${value} ${value == arr.at(-1) ? "'s dogs eat too much!" : "and"}`
+  );
+});
+
+ownersEatTooLittle.forEach((value, _, arr) => {
+  console.log(
+    `${value} ${value == arr.at(-1) ? "'s dogs eat too little!" : "and"}`
+  );
+});
+
+console.log(dogs);
+console.log(stringValue);
