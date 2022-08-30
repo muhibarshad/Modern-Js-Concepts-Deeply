9eafc1a7c82d52f2b93f220ee6b6385d7ae99965
Muhib Arshad <muhibarshad123@gmail.com>
2022-08-30 23:03:01 +0500

    Bankist Dates portion added

---
+++ Step_10_Dates_numbers_timers/dateAndTime.js
@@ -0,0 +1,33 @@
+'use strict';
+
+//To get todays date and time
+console.log(new Date);
+
+//Parsing the strings
+console.log(new Date('Tue Aug 30 2022'));
+console.log(new Date('August 14,2022'));
+console.log(new Date(2022,8,12,7,30,15));//Months in the JS are zero based
+
+//TimeStamp
+console.log(new Date(0));//Give the initial Time
+console.log(3*24*60*60*1000);//millSeconds after the 3 days of timeStamp
+
+//Methods in the dates
+const today=new Date();
+console.log(today);
+console.log(today.getFullYear());
+console.log(today.getMonth());//zero based
+console.log(today.getDate());//gives day no
+console.log(today.getDay());//give day of the week
+console.log(today.getHours());
+console.log(today.getMinutes());
+console.log(today.getSeconds());
+console.log(today.getMilliseconds());
+console.log(today.toISOString());//standard time zone format
+console.log(today.getTime());//gives millseconds after the timeStamp(1661878587016)
+console.log(new Date(today.getTime()));
+console.log(Date.now());//same gives the TimeStamp
+
+//We can aslo change the time(by using set)
+today.setFullYear(2023);
+console.log(today);
--- Step_9_Arrays/Jones_Schedtmen_project_Bankist_starter/script.js
+++ Step_9_Arrays/Jones_Schedtmen_project_Bankist_starter/script.js
@@ -1,15 +1,32 @@
 'use strict';
 
-/////////////////////////////////////////////
-/////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
 // BANKIST APP
 
+/////////////////////////////////////////////////
 // Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,23 +34,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -85,11 +101,21 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (mov, i) {
     const typeMov = mov > 0 ? 'deposit' : 'withdrawal';
 
+    let movDate = new Date(acc.movementsDates[i]);
+    let year = movDate.getFullYear();
+    let month = (movDate.getMonth() + 1).toString().padStart(2, 0);
+    let date = movDate.getDate().toString().padStart(2, 0);
+    let hours = movDate.getHours().toString().padStart(2, 0);
+    let minutes = movDate.getMinutes().toString().padStart(2, 0);
+    let seconds = movDate.getSeconds().toString().padStart(2, 0);
+
+    let displayDate = `${date}/${month}/${year}`;
     let html = `<div class="movements__row">
       <div class="movements__type movements__type--${typeMov}">${
       i + 1
     } ${typeMov}</div>
-      <div class="movements__value">${mov}€</div>
+    <div class="movements__date">${displayDate}</div>
+      <div class="movements__value">${Math.abs(mov.toFixed(2))}€</div>
     </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -111,7 +137,7 @@ createUserName(accounts);
 //calculate the balance
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 /*incomes,outcomes,interest->Display*/
@@ -120,19 +146,19 @@ const calDisplaySummary = function (acc) {
   const income = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${income}€`;
+  labelSumIn.textContent = `${income.toFixed(2)}€`;
   //outcome
   const outcome = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(outcome)}€`;
+  labelSumOut.textContent = `${Math.abs(outcome).toFixed(2)}€`;
   //interst
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => deposit * (acc.interestRate / 100))
     .filter(deposit => deposit >= 1) //if he/she deposit greater than 1 EURO so he/she gets the interest
     .reduce((int, mov) => int + mov, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const updateUI = function (acc) {
@@ -147,8 +173,19 @@ btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
   currentUser = accounts.find(acc => acc.userName === inputLoginUsername.value);
 
-  if (currentUser?.pin === Number(inputLoginPin.value)) {
+  if (currentUser?.pin === +inputLoginPin.value) {
     containerApp.style.opacity = 100;
+
+    //computing dates
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = (now.getMonth() + 1).toString().padStart(2, 0);
+    let date = now.getDate().toString().padStart(2, 0);
+    let hours = now.getHours().toString().padStart(2, 0);
+    let minutes = now.getMinutes().toString().padStart(2, 0);
+    let seconds = now.getSeconds().toString().padStart(2, 0);
+    labelDate.textContent = `${date}/${month}/${year} , ${hours} : ${minutes}`;
+
     labelWelcome.textContent = `Welcome ,${currentUser.owner.split(' ')[0]}`;
 
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -161,7 +198,7 @@ btnLogin.addEventListener('click', function (e) {
 //Transfering_Money------Activity
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  let amount = Number(inputTransferAmount.value);
+  let amount = +inputTransferAmount.value;
   let recevierAcc = accounts.find(
     acc => acc.userName === inputTransferTo.value
   );
@@ -177,6 +214,10 @@ btnTransfer.addEventListener('click', function (e) {
   ) {
     recevierAcc.movements.push(amount);
     currentUser.movements.push(-amount);
+
+    currentUser.movementsDates.push(new Date().toISOString());
+    recevierAcc.movementsDates.push(new Date().toISOString());
+
     updateUI(currentUser);
   }
 });
@@ -184,12 +225,14 @@ btnTransfer.addEventListener('click', function (e) {
 //Request_Loan------Activity
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  let amount = Number(inputLoanAmount.value);
+  let amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentUser.movements.some(mov => mov >= amount * 0.1)) {
     inputLoanAmount.value = '';
     inputLoanAmount.blur();
 
     currentUser.movements.push(amount);
+    currentUser.movementsDates.push(new Date().toISOString());
+
     updateUI(currentUser);
   }
 });
@@ -199,7 +242,7 @@ btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
     inputCloseUsername.value === currentUser.userName &&
-    Number(inputClosePin.value) === currentUser.pin
+    +inputClosePin.value === currentUser.pin
   ) {
     inputClosePin.value = inputCloseUsername.value = '';
     containerApp.style.opacity = 0;
