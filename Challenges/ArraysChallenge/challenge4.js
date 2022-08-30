91c27596860bc2c2451c369fc0679a68bf5a8df2
Muhib Arshad <muhibarshad123@gmail.com>
2022-08-30 20:29:08 +0500

    Challenge 4 complete

--- Challenges/ArraysChallenge/challenge4.js
+++ Challenges/ArraysChallenge/challenge4.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//challeneg Arrays :04
 const dogs = [
   { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
   { weight: 8, curFood: 200, owners: ["Matilda"] },
@@ -8,13 +9,75 @@ const dogs = [
 ];
 
 //1-
-dogs.forEach(dog=>dog.recommendedFood=Math.trunc(dog.weight**0.75*28));
+dogs.forEach((value) => {
+  value.recommendedFood = Math.trunc(value.weight ** 0.75 * 28);
+});
+
 //2-
+let str = (na, str, cur, rec) => {
+  na = na.replace(na[0], na[0].toUpperCase());
+  console.log(
+    `${na} dog's is eating too ${str} beacuse its current food is ${cur} but the recommended food is ${rec}`
+  );
+};
+dogs.find((value) => {
+  let x = value.owners
+    .filter((nam) => nam === "Sarah")
+    .join("")
+    .toLowerCase();
+  x === "sarah" &&
+    value.curFood > value.recommendedFood &&
+    str(x, "much", value.curFood, value.recommendedFood);
+  x === "sarah" &&
+    value.curFood < value.recommendedFood &&
+    str(x, "little", value.curFood, value.recommendedFood);
+});
 
+//3-
+const ownersEatTooMuch = dogs
+  .filter((value) => value.curFood > value.recommendedFood)
+  .flatMap((value) => value.owners);
+const ownersEatTooLittle = dogs
+  .filter((value) => value.curFood < value.recommendedFood)
+  .flatMap((value) => value.owners);
 
+//4-
+ownersEatTooMuch.forEach((value, _, arr) => {
+  console.log(
+    `${value} ${value == arr.at(-1) ? "'s dogs eat too much!" : "and"}`
+  );
+});
+ownersEatTooLittle.forEach((value, _, arr) => {
+  console.log(
+    `${value} ${value == arr.at(-1) ? "'s dogs eat too little!" : "and"}`
+  );
+});
 
+//5-
+const eatingExactly = dogs.some(
+  (value) => value.curFood === value.recommendedFood
+);
+console.log(eatingExactly);
 
+//6-
+const eatingOkay = dogs.some(
+  (value) =>
+    value.curFood > value.recommendedFood * 0.9 &&
+    value.curFood < value.recommendedFood * 1.1
+);
+console.log(eatingOkay);
 
-document.querySelector('.btn').addEventListener('click',function(){
-    console.log(dogs);
-})
+//7-
+const dogsEatingOkay = dogs.filter(
+  (value) =>
+    value.curFood > value.recommendedFood * 0.9 &&
+    value.curFood < value.recommendedFood * 1.1
+);
+console.log(dogsEatingOkay);
+
+//8-
+console.log(dogs);
+const sortedDogs = dogs
+  .map((x) => x)
+  .sort((a, b) => a.recommendedFood - b.recommendedFood);
+console.log(sortedDogs);
