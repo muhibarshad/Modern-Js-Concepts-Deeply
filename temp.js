cb5fb3f07925ac5797f5910d07c9284a7145c1d6
Muhib Arshad <muhibarshad123@gmail.com>
2022-10-05 03:01:38 +0500

    Sets Revision

---
+++ temp.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+// let setUSernames=new Set(['Haider','Ali','Muhib','Ali','Hamza','Ali']);
+
+// le//t x;
+// x=setUSernames.size;
+// x=setUSernames.has('Ali');
+// setUSernames.delete('Ali');
+// setUSernames.add('Ali');
+// setUSernames.clear();
+// console.log(setUSernames);
+
+//Maps
+
+
