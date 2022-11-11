728f94980609018e98a817043de8f0c37fe1280e
muhib7353 <muhibarshad123@gmail.com>
2022-11-11 05:10:31 +0500

    add:chainingPromises

---
+++ Step_13_ Asynchronous JavaScript_ Promises,_Async_Await, and AJAX/fetchAPI_promises.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const countriesCard = document.querySelector(".countries");
+const displayingCountry = function (data, className = "") {
+  const html = `
+      <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} M</p>
+        <p class="country__row"><span>🗣️</span>${
+          Object.values(data.languages)[1]
+            ? Object.values(data.languages)[1]
+            : Object.values(data.languages)[0]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          Object.values(data.currencies)[0].name
+        }</p>
+      </div>
+    </article>
+      `;
+  countriesCard.insertAdjacentHTML("beforeend", html);
+  countriesCard.style.opacity = 1;
+};
+
+//Promise-code
+const getCountryData = function (countryName) {
+  fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((response) => response.json())
+    .then((data) => {
+      displayingCountry(data[0]);
+    });
+};
+getCountryData("Pakistan");
+getCountryData("UAE");
