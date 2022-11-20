16b0d52b8426e5ec20ab39a5a309ffedc3ebab0d
muhib7353 <muhibarshad123@gmail.com>
2022-11-20 05:19:11 +0500

    add:Modules

--- Step_14_Modules,NPM,BABEL,BUGS SOLVING/mainScript.js
+++ Step_14_Modules,NPM,BABEL,BUGS SOLVING/mainScript.js
@@ -1,5 +1,14 @@
 //Importing files
-import './shoppingCart_module.js'
+// import "./shoppingCart_module.js";
+// import { addTocart } from "./shoppingCart_module.js";
+// import { addTocart as cart } from "./shoppingCart_module.js";
+// import name from './shoppingCart_module.js'
+// import { addTocart ,carts,shippingCost } from "./shoppingCart_module.js";
+// import name,{ addTocart ,carts,shippingCost } from "./shoppingCart_module.js";
+import * as ShoppingCart from './shoppingCart_module.js'
 
-console.log('Importing File')
-
+console.log("Importing File");
+ShoppingCart.addTocart("T-Shirts", 2);
+ShoppingCart.addTocart("Paints", 8);
+ShoppingCart.addTocart("shawal", 9);
+console.log(ShoppingCart.carts);
--- Step_14_Modules,NPM,BABEL,BUGS SOLVING/shoppingCart_module.js
+++ Step_14_Modules,NPM,BABEL,BUGS SOLVING/shoppingCart_module.js
@@ -1,10 +1,14 @@
 //Exporting Files
 console.log("Exporting File");
 
+const shippingCost = 10;
 const shopName = "Muhib Garments";
-const carts = [];
+export let carts = [];
 
 export const addTocart = function (product, quantity) {
   carts.push({ product, quantity });
-  console.log(`You are ordering ${quantity} of ${product} from ${shopName} `);
+  console.log(`You are ordering ${quantity} of ${product} from ${shopName}`);
 };
+
+export default "ali";
+// export{carts as cartsArr,shippingCost,shopName}
